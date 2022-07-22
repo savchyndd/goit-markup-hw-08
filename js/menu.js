6386eaaebd155877645277b6970c1503b5dffc39
Dmytro <savchyndd@gmail.com>
2022-07-22 15:33:17 +0300

    add mobile style

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,15 @@
-(() => {
-    const refs = {
-      menuBtn: document.querySelector("[data-menu-btn]"),
-      menu: document.querySelector("[data-menu]"),
-    };
-  
-    refs.menuBtn.addEventListener("click", toggleMenu);
-   
-    function toggleMenu() {
-      refs.menuBtn.classList.toggle("is-open");
-      refs.menu.classList.toggle("is-open");
-    }
-  })();
+
+  (() => {
+  const menuBtnRef = document.querySelector("[data-menu-btn]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
